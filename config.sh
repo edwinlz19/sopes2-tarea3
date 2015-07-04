1fed2cd4bbe404f1bc657e905a2acbe413b9d308
edwinlz19 <edwinlz19@hotmail.com>
2015-07-04 07:54:48 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir /home/edwin/servicioHTTP
 cp servicio.py /home/edwin/servicioHTTP
-cp deamon-servicioHTTP /etc/init.d
+cp dservicioHTTP /etc/init.d
 
-update-rc.d deamon-servicioHTTP defaults
+update-rc.d dservicioHTTP defaults
 
 echo "Se ha terminado ir a localhost:8000"

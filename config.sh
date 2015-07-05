678aecea1447425a63d6488dc0436ba9f834d446
edwinlz19 <edwinlz19@hotmail.com>
2015-07-04 22:54:54 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 mkdir /home/edwin/servicioHTTP
 cp servicio.py /home/edwin/servicioHTTP
 cp dservicioHTTP /etc/init.d
-
+chmod ug+x /etc/init.d/dservicioHTTP
 update-rc.d dservicioHTTP defaults
 
 echo "Se ha terminado, puede iniciar el servicio"
